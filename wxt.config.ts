944c132bdcb01b08581770db0b2dd9d73ca181dd
bremmdev <bremmdev@gmail.com>
2025-08-11 11:00:54 +0200

    add metadata and icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
+    default_locale: "en",
+    name: "Link Manager",
     permissions: ["tabs", "scripting", "storage"],
   },
   modules: ["@wxt-dev/module-react"],
