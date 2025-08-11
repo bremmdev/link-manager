d0c8fdf5d7f34def9c848e8e7568ed47336c547f
bremmdev <bremmdev@gmail.com>
2025-08-11 20:03:20 +0200

    rm default locale

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    default_locale: "en",
     name: "Link Manager",
     permissions: ["tabs", "scripting", "storage"],
   },
